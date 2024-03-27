92d6a3abcc52df45aca6a32f36e95f9edc6ef783
Etyre <elityre@gmail.com>
2024-03-27 13:31:49 -0700

    update to the prompt

--- src/promptText.js
+++ src/promptText.js
@@ -1,17 +1,17 @@
 const promptForJankyVersion = `I’m making flashcards to review material that I’m reading. I’m going to give you a selection of text. Please read this selection, extract the important ideas and interesting facts, and make flash cards for each.
 
-All of the flashcards should be formatted as bullets, with the answer in a nested bullet below the question. Every question should have the tag “#ankify” at the end.
+All of the flashcards should be formatted as bullets, with the answer in a nested bullet below the question. 
 
 Here are some example flashcards:
 
-* What percentage of American women were working in offices in 1900? #ankify
-   * About 20%
+- What percentage of American women were working in offices in 1900? 
+   - About 20%
 
-* How was it regarded if a woman was working in an office in 1900? #ankify
-   * It was lamented as an unfortunate result of financial stress, and slightly shameful to her father.
+- How was it regarded if a woman was working in an office in 1900? 
+   - It was lamented as an unfortunate result of financial stress, and slightly shameful to her father.
 
-* Why was it lamentable for a woman to be working in an office in 1900? #ankify
-   * Because this would subject them to temptations of being seduced.
+- Why was it lamentable for a woman to be working in an office in 1900? 
+   - Because this would subject them to temptations of being seduced.
 
 
 Some rules for formulating questions:
@@ -20,22 +20,22 @@ Some rules for formulating questions:
 
 Instead of a question like... 
 
-* Was the public enthusiastic about the war during World War II? #ankify
-    * No, the public was not very gung ho or enthusiastic about it.
+- Was the public enthusiastic about the war during World War II? 
+    - No, the public was not very gung ho or enthusiastic about it.
 
 ...produce a question like...
 
-* What was the attitude of the American public towards World War II? #ankify
-    * Determined to do what needs to be done, but without enthusiasm or patriotic fervor. 
+- What was the attitude of the American public towards World War II? 
+    - Determined to do what needs to be done, but without enthusiasm or patriotic fervor. 
 
 Instead of...
 
-* Were wages frozen during WWII? #ankify
-    * Yes.
+- Were wages frozen during WWII? 
+    - Yes.
 
 ...write...
 
-- What happen to wages during WWII? #ankify
+- What happen to wages during WWII? 
     - Wages were frozen.
 
 
@@ -43,32 +43,32 @@ Instead of...
 
 Instead of...
 
-- What is the purpose of node's "watch mode" in node? #ankify
+- What is the purpose of node's "watch mode" in node? 
     -  It automatically reruns a file when changes are made.
 
 ...the question should be...
 
-- What does node's "watch mode" do? #ankify
+- What does node's "watch mode" do? 
     -  It automatically reruns a file when changes are made.
 
 Instead of...
 
-- What is the objective of an "upsert" operation? #ankify
+- What is the objective of an "upsert" operation? 
     -  The objective is to save data in the database, regardless if it already exists or not.
 
 ...write...
 
-- What does the "upsert" operation do? #ankify
+- What does the "upsert" operation do? 
     -  Save data in the database, regardless if it already exists or not.
 
 Instead of...
 
-- What action does Fetch perform? #ankify
+- What action does Fetch perform? 
     -  It sends an HTTP request.
 
 ...write...
 
-- What does Fetch do? #ankify
+- What does Fetch do? 
     -  It sends an HTTP request.
 
 
@@ -91,22 +91,22 @@ const extaBitAboutClozes = `Alternatively, cards can be a single bullet point wi
 
 Some examples of cloze cards:
 
-* The Holy Alliance was composed of {1:Russia}, {1:Prussia}, and {1:Austria}. #ankify 
+* The Holy Alliance was composed of {1:Russia}, {1:Prussia}, and {1:Austria}.  
 
-* The quadruple alliance was composed of {1:Austria}, {1:Prussia}, {1: Russia}, and {1:Great Britain}. #ankify 
+* The quadruple alliance was composed of {1:Austria}, {1:Prussia}, {1: Russia}, and {1:Great Britain}.  
 
-* The congress system worked pretty well for {1:avoiding Great Power war}, for about {1:100 years}. #ankify
+* The congress system worked pretty well for {1:avoiding Great Power war}, for about {1:100 years}. 
 
-* The nation of Belgium was created at {1:the Congress of Vienna}. #ankify
+* The nation of Belgium was created at {1:the Congress of Vienna}. 
 
-* In WWI, the British promised the Arabs {1:Independence} in return for {1:rebelling against the Turks}. #ankify
+* In WWI, the British promised the Arabs {1:Independence} in return for {1:rebelling against the Turks}. 
 
-* In 1900, a girl who set out to earn money was {1:embarrassing her father} by implying {1:that he couldn't support her}. #ankify
+* In 1900, a girl who set out to earn money was {1:embarrassing her father} by implying {1:that he couldn't support her}. 
 
-* In 1900, women would never be present in a {1:bar} or {1:smoking train-car}. #ankify
+* In 1900, women would never be present in a {1:bar} or {1:smoking train-car}. 
 
-* In an American city in 1900, {1:horses} were everywhere. #ankify
+* In an American city in 1900, {1:horses} were everywhere. 
 
-* In 1900, people went without {fresh fruit and vegetables} for most of the year. #ankify
+* In 1900, people went without {fresh fruit and vegetables} for most of the year. 
 
 The cloze should be selected so that there's a unique corect answer (instead of several ways to fill in then blank that would still be correct), and open ended enough that the answer isn't obvious from the rest fo the sentence.`;
